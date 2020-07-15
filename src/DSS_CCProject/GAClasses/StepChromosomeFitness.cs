9929ca7cae22f4ee23fe7fca8cdf67af377b7df8
geff10 <esse.zs10@gmal.com>
2020-07-16 00:13:40 +0200

    Create Fitness

---
+++ src/DSS_CCProject/GAClasses/StepChromosomeFitness.cs
@@ -0,0 +1,24 @@
+﻿using GeneticSharp.Domain.Chromosomes;
+using GeneticSharp.Domain.Fitnesses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSS_CCProject.GAClasses
+{
+    public class StepChromosomeFitness : IFitness
+    {
+        private double fullPenalty;
+        public double Evaluate(IChromosome chromosome)
+        {
+            fullPenalty = 0;
+            Simulate(chromosome as StepChromosome);
+            return fullPenalty;
+        }
+
+        private void Simulate(StepChromosome genome)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
