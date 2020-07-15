f7dfd40e4714e6f03b0f4b3dcca5b8355e3758eb
geff10 <esse.zs10@gmal.com>
2020-07-15 23:12:44 +0200

    Create skeleton of own chromosome representation

---
+++ src/DSS_CCProject/GAClasses/StepChromosome.cs
@@ -0,0 +1,28 @@
+﻿using GeneticSharp.Domain.Chromosomes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSS_CCProject.GAClasses
+{
+    public class StepChromosome : ChromosomeBase
+    {
+        public StepChromosome() : base(1)
+        {
+        }
+
+        public StepChromosome(int length) : base(length)
+        {
+        }
+
+        public override IChromosome CreateNew()
+        {
+            return new StepChromosome();
+        }
+
+        public override Gene GenerateGene(int geneIndex)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
