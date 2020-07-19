9d0832a7d6586013f5f7e3e74d051931c82bee97
geff10 <esse.zs10@gmal.com>
2020-07-19 21:58:20 +0200

    Add Machine with type

---
+++ src/DSS_CCProject/Consts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSS_CCProject
+{
+    public class Consts
+    {
+        public enum MachineType
+        {
+            Cutting = 0,
+            Blending = 1,
+            Welding = 2,
+            Testing = 3,
+            Painting = 4,
+            Packing = 5
+        }
+    }
+}
---
+++ src/DSS_CCProject/Machine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static DSS_CCProject.Consts;
+
+namespace DSS_CCProject
+{
+    class Machine
+    {
+        long id;
+        MachineType machineType;
+
+        public long Id { get => id; set => id = value; }
+        public MachineType MachineType { get => machineType; set => machineType = value; }
+    }
+}
